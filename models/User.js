680f9212d3574f2d17f068407090763cc1ecea80
adairconlin <adair.conlin@gmail.com>
2022-02-08 13:53:20 -0500

    Created User and Thought schemas

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+let validateEmail = function(email) {
+    let emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(email);
+};
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        }
+    },
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, "Please use a valid email address"]
+        }
+    },
+    {
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        }]
+    },
+    {
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
