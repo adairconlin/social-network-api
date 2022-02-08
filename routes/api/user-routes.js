1c95b09dd648d1fc330587ac98748707b40793e3
adairconlin <adair.conlin@gmail.com>
2022-02-08 14:38:47 -0500

    Finish /users routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const {
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 } = require("../../controllers/user-controller");
 
 router
     .route("/")
-    .get(getAllUsers);
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route("/:id")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
